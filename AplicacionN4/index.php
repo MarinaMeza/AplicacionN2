0ad0b5c1851c897eecaee558ee13748d38514b2f
Marina Meza <Marina Meza>
2017-03-24 17:51:55 -0300

    Ejercicio 4 Segundo commit

--- AplicacionN4/index.php
+++ AplicacionN4/index.php
@@ -1,3 +1,21 @@
 <?php
+$operador='/';
+$op1=rand(1,15);
+$op2=rand(1,15);
 
+switch ($operador)
+{
+    case '+':
+        echo $op1+$op2;
+        break;
+    case '-':
+        echo $op1-$op2;
+        break;
+    case '*':
+        echo $op1*$op2;
+        break;
+    case '/':
+        echo $op1/$op2;
+        break;
+}
 ?>
