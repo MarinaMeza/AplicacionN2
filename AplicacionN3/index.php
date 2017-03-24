563042401d0fb6955cc0a10f4f1a1fc5b801b40c
Marina Meza <Marina Meza>
2017-03-24 17:41:12 -0300

    Ejericio 3 Segundo commit

--- AplicacionN3/index.php
+++ AplicacionN3/index.php
@@ -1,3 +1,23 @@
 <?php
+$a=rand(1,15);
+$b=rand(1,15);
+$c=rand(1,15);
+
+if(($a<$b && $a>$c) || ($a>$b && $a<$c))
+{
+    echo $a;
+}
+elseif (($b<$c && $b>$a) || ($b>$c && $b<$a))
+{
+    echo $b;
+}
+elseif (($c<$a && $c>$b) || ($c>$a && $c<$b))
+{
+    echo $c;
+}
+elseif ($a==$b || $b==$c || $a==$c)
+{
+    echo "No hay valor del medio";
+}
 
 ?>
