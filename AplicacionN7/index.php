ce37a7631f8811040da63c5d394069e97bfcd481
MarinaMeza <kaiodelnorte@gmail.com>
2017-03-31 23:26:38 -0300

    Ejercicio 7 Segundo commit

--- AplicacionN7/index.php
+++ AplicacionN7/index.php
@@ -1,5 +1,39 @@
 <?php
 
+    $array = array();
+    
+    for($i=0;$i<10;$i++)
+    {
+        if(($i%2)!=0)
+        {
+            array_push($array,$i);
+        }
+    }
+    
+    echo "for <br>";
+    
+    for($i=0;$i<count($array);$i++)
+    {
+        echo $array[$i]."<br>";
+    }
+    
+    echo "while <br>";
+    
+    $i=0;
+    while($i!=count($array))
+    {
+        echo $array[$i]."<br>";
+        $i++;
+    }
+    
+    echo "foreach <br>";
+    
+    foreach ($array as $key)
+    {
+        echo $key."<br>";
+    }
+
+
 
 
 ?>
