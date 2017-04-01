996066e52affd4feff1f5dc2cda9988a87b03640
MarinaMeza <kaiodelnorte@gmail.com>
2017-03-31 23:53:49 -0300

    Ejercicio 9 Segundo commit

--- AplicacionN9/index.php
+++ AplicacionN9/index.php
@@ -1,5 +1,41 @@
 <?php
 
-    
+    $lapicera1 = array(
+        'color' => 'azul',
+        'marca' => 'bic',
+        'trazo' => 'grueso',
+        'precio' => '$10',
+    );
 
+    $lapicera2 = array(
+        'color' => 'negro',
+        'marca' => 'pelikan',
+        'trazo' => 'fino',
+        'precio' => '$12',
+    );
+
+    $lapicera3 = array(
+        'color' => 'rojo',
+        'marca' => 'faber castell',
+        'trazo' => 'grueso',
+        'precio' => '$9',
+    );
+
+    echo "Lapicera 1:<br>";
+    foreach ($lapicera1 as $key)
+    {
+        echo $key."<br>";
+    }
+
+    echo "Lapicera 2:<br>";
+    foreach ($lapicera2 as $key)
+    {
+        echo $key."<br>";
+    }
+
+    echo "Lapicera 3:<br>";
+    foreach ($lapicera3 as $key)
+    {
+        echo $key."<br>";
+    }
 ?>
