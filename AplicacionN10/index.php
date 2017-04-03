56cee9cae3e39b6aeb9e82e96207d849d7a88718
MarinaMeza <kaiodelnorte@gmail.com>
2017-04-03 16:18:30 -0300

    Ejercicio 10 Segundo commit

--- AplicacionN10/index.php
+++ AplicacionN10/index.php
@@ -1,5 +1,41 @@
 <?php
 
-    
+    $i=1;
+    //$lapicera = array('color','marca','trazo','precio');
+    $arrayLapiceras=array(
+        'lapicera 1' => array(
+            'color' => 'azul',
+            'marca' => 'bic',
+            'trazo' => 'grueso',
+            'precio' => '$10'
+        ),
 
+        'lapicera 2' => array(
+            'color' => 'negro',
+            'marca' => 'pelikan',
+            'trazo' => 'fino',
+            'precio' => '$12'
+        ),
+
+        'lapicera 3' => array(
+            'color' => 'rojo',
+            'marca' => 'faber castell',
+            'trazo' => 'grueso',
+            'precio' => '$9'
+        )
+    );
+
+    //var_dump($arrayLapiceras);
+    /*foreach ($arrayLapiceras as $key => $value) {
+        echo $key
+    }*/
+    foreach ($arrayLapiceras as $key)
+    {
+        echo "Lapicera ".$i++."<br>";
+        foreach ($key as $key)
+        {
+            echo $key;
+            echo "<br>";
+        }
+    }
 ?>
