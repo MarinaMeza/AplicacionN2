43af07f2a379716cb8be1377200959ba02f8624a
MarinaMeza <kaiodelnorte@gmail.com>
2017-05-06 18:14:32 -0300

    Ejercicio 14 Segundo commit

--- AplicacionN14/index.php
+++ AplicacionN14/index.php
@@ -1,3 +1,27 @@
 <?php
+    function esPar($pNumero){
+        $rta;
 
+        if($pNumero%2==0){
+            $rta = TRUE;
+        }else{
+            $rta = FALSE;
+        }
+
+        return $rta;
+    }
+
+    function esImpar($pNumero){
+        $rta;
+
+        if($pNumero%2==0){
+            $rta = FALSE;
+        }else{
+            $rta = TRUE;
+        }
+
+        return $rta;
+    }
+    
+    esPar(7);
 ?>
